db439032587942e20a5ddc706a6da21ec929c496
nagaraju myaka <nmmyaka@amazon.com>
2022-02-21 16:00:57 -0500

    updated codebuild command

--- lib/index.ts
+++ lib/index.ts
@@ -58,7 +58,7 @@ export class PipelineStack extends Stack {
         ],
         commands: [
           'npm ci',
-          'npm run build',
+          'npx cdk build',
           'npx cdk synth'
         ]
       }
